1530a82dee322da909081caede88d1e09afec9ae
Miten <m.manish.mm3@gmail.com>
2023-01-20 09:30:28 +0530

    Login Completed

---
+++ lib/contants/text_strings.dart
@@ -0,0 +1,17 @@
+//profile screen
+
+const String tProfile="Profile";
+const String tEditprofile="Edit Profile";
+const String tLogoutDialogHeading="Logout";
+const String tProfileHeading="DocHub";
+const String tProfileSubHeading="Miten Mithaiwala";
+//menu
+const String tMenu5="Logout";
+const String tMenu1="Settings";
+const String tMenu4="Information";
+const String tMenu2="Billing Details";
+const String tMenu3="User Management";
+//    upate profile screen
+const String tDelete="Delete";
+const String tJoined="Joined";
+const String tJoinDate="31 October 2002";
